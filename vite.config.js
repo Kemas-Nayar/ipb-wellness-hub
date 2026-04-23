a8a745eb2d60e06ca1b06264e6a9595dffc6bcc0
Kemas Nayar <kem4s2422@gmail.com>
2026-04-23 12:37:59 +0700

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    strictPort: true, // Opsional: Agar Vite error kalau port 3000 dipakai app lain, bukan malah pindah ke 3001
+  }
 })
