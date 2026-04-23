13da28cc0d40b969475cef2ac976fee2989f4a21
Kemas Nayar <kem4s2422@gmail.com>
2026-04-23 19:25:38 +0700

    test bentar 2

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
-    host: "0.0.0.0",
+    host: true,
     strictPort: true, // Opsional: Agar Vite error kalau port 3000 dipakai app lain, bukan malah pindah ke 3001
+  },
+  build: {
+    chunkSizeWarningLimit: 1600, // Menghilangkan warning chunk tadi
+    sourcemap: true // Biar kalau error di HP, kelihatan baris kodenya di mana
   }
 })
